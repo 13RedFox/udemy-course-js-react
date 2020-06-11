e55d323cdefd7eedd4827edbf266779d78572ee1
Serhii <sergei181989@gmail.com>
2020-06-11 21:30:53 +0300

    Work in home task to 001 + added gitignore file.

---
+++ 001/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const questionOne = prompt('Один из последних просмотренных фильмов?', ''),
+      questionTwo = prompt('На сколько оцените его?', ''),
+      questionThree = prompt('Один из последних просмотренных фильмов?', ''),
+      questionFour = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[questionOne] = questionTwo;
+personalMovieDB.movies[questionThree] = questionFour;
+
+console.log(personalMovieDB);
