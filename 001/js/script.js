f76e9fd23457463af267cc09bcce87580b6aec1b
Serhii <sergei181989@gmail.com>
2020-06-12 11:28:09 +0300

    Done next home task in 001 directory.

--- 001/js/script.js
+++ 001/js/script.js
@@ -10,12 +10,28 @@ const personalMovieDB = {
   privat: false,
 };
 
-const questionOne = prompt('Один из последних просмотренных фильмов?', ''),
-      questionTwo = prompt('На сколько оцените его?', ''),
-      questionThree = prompt('Один из последних просмотренных фильмов?', ''),
-      questionFour = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const questionOne = prompt('Один из последних просмотренных фильмов?', ''),
+        questionTwo = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[questionOne] = questionTwo;
-personalMovieDB.movies[questionThree] = questionFour;
+  if (questionOne !== null && questionTwo !== null && questionOne !== '' && questionTwo !== '' && questionOne.length < 50) {
+
+    personalMovieDB.movies[questionOne] = questionTwo;
+    console.log('Done!');
+  } else {
+    console.log('Error');
+    i--;
+  }
+};
+
+if (personalMovieDB.count < 10) {
+  console.log('Просмотренно довольно мало фильмов.');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель.');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман!');
+} else {
+    console.log('Произошла ошибка!!!');
+}
 
 console.log(personalMovieDB);
